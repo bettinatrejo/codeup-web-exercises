8535e127d6d9129401bd81cd462cfd562e78b344
Bettina Trejo <bettina.trejo12@gmail.com>
2018-01-25 16:37:13 -0600

    testing javascript

---
+++ js/external.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log("Hello from external JavaScript");
+
+alert("Welcome to my Website!");
+
+var favoriteColor = prompt("What is your favorite color?");
+    console.log("Great, " + favoriteColor + " is my favorite color too!");
