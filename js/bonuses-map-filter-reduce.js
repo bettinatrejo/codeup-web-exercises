7c8d67c0acf9b9f2b59b1648daa0fadf734009db
Bettina Trejo <bettina.trejo12@gmail.com>
2018-02-22 10:50:44 -0600

    bonus exercise

--- js/bonuses-map-filter-reduce.js
+++ js/bonuses-map-filter-reduce.js
@@ -36,13 +36,18 @@ console.log(firstLetters);
 // PROBLEM 2 - create array of user objects based on the customers array
 // of objects (each user object should just have name and age properties)(use .map())
 
+//
+// let properties = customers.map(customer => {
+//     return {name: customer.name, age: customer.age}});
 
-let properties = customers.map(customer => {
-    return {name: customer.name, age: customer.age}});
+
+let properties = customers.map(({ name, age }) => {
+    return {
+        name,
+        age
+    };
+});
 console.log(properties);
-
-
-
 
 
 
@@ -51,7 +56,7 @@ console.log(properties);
 // containing the same properties as the objects on the customers objects(use .filter())
 
 
-let civilArray = customers.filter(function(customer) {
+let civilArray = customers.filter(customer => {
     return customer.occupation === "Teacher" || customer.occupation === "Police Officer";
     });
 console.log(civilArray);
@@ -66,3 +71,7 @@ let average = customers.reduce((total, customer) => {
     return total + customer.age / customers.length;
 },0);
 console.log(average);
+
+
+
+fetch
